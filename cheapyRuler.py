94c215d1d41295fd3f4aa339190ff980629876b0
Daniel <danielelijahsmith@gmail.com>
2016-05-05 22:12:40 -0700

    Initial commit. First few functions ported from cheap Ruler
    cheap ruler here https://github.com/mapbox/cheap-ruler

---
+++ cheapyRuler.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+#coding:utf-8
+"""
+  Author:  Daniel Smith --<>
+  Purpose: Python Port of Mapbox Cheap Ruler (https://github.com/mapbox/cheap-ruler)
+  Created: 5/5/2016
+"""
+
+import math
+
+factors = {'kilometers': 1,
+        'miles': 1000 / 1609.344,
+        'nauticalmiles': 1000 / 1852,
+        'meters': 1000,
+        'metres': 1000,
+        'yards': 1000 / 0.9144,
+        'feet': 1000 / 0.3048,
+        'inches': 1000 / 0.0254}
+
+########################################################################
+class cheapyRuler():
+    """python port of mapbox cheap ruler. not all functions are available"""
+
+    #----------------------------------------------------------------------
+        
+    def __init__(self,lat,units):
+        """Constructor"""
+        m = factors[units]
+        cos = math.cos(lat * math.pi / 180);
+        cos2 = 2 * cos * cos - 1;
+        cos3 = 2 * cos * cos2 - cos;
+        cos4 = 2 * cos * cos3 - cos2;
+        cos5 = 2 * cos * cos4 - cos3;        
+        self.kx = m * (111.41513 * cos - 0.09455 * cos3 + 0.00012 * cos5);
+        self.ky = m * (111.13209 - 0.56605 * cos2 + 0.0012 * cos4);
+        
+    
+    def distance(self,a,b):
+        ''' Distance between points'''
+        dx = (a[0] - b[0]) * self.kx
+        dy = (a[1] - b[1]) * self.ky
+        return math.sqrt(dx * dx + dy * dy)
+    
+    def destination(self,p,dist,bearing):
+        '''new point from point distance and bearing'''
+        a = (90 - bearing) * math.pi / 180
+        return [p[0] + math.cos(a) * dist / self.kx,
+            p[1] + math.sin(a) * dist / self.ky]
+    
+    def bearing(self,a,b):
+        '''bearing to from point to point'''
+        dx = (b[0] - a[0]) * self.kx
+        dy = (b[1] - a[1]) * self.ky
+        if dx == 0 and dy == 0:
+            return 0
+        bearing = math.atan2(-dy, dx) * 180 / math.pi + 90
+        if bearing > 180:
+            bearing -= 360
+        return bearing
